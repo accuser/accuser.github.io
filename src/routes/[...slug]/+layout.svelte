a7156c09b48cf4108108d951be12dd17612ca390
Matthew Gibbons <mhgibbons@me.com>
2024-11-17 20:34:47 +0000

    Add Footer component to layout for improved site structure

--- src/routes/[...slug]/+layout.svelte
+++ src/routes/[...slug]/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Footer from '$lib/components/Footer.svelte';
 	import type { Snippet } from 'svelte';
 	import type { PageData } from './$types';
 
@@ -20,3 +21,5 @@
 	</main>
 	<footer></footer>
 </article>
+
+<Footer />

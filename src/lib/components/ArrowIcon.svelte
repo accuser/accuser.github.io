0c5ac0f5c9708f09fb8d429c9c97ccc8c1546a3a
Matthew Gibbons <mhgibbons@me.com>
2024-11-17 20:34:18 +0000

    Add new social media icons and update ArrowIcon component

--- src/lib/components/ArrowIcon.svelte
+++ src/lib/components/ArrowIcon.svelte
@@ -4,11 +4,11 @@
 	let props: SVGAttributes<SVGElement> = $props();
 </script>
 
-<svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
-	<path
+<svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}
+	><path
 		stroke="currentColor"
 		stroke-linecap="round"
 		stroke-linejoin="round"
 		d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"
-	/>
-</svg>
+	/></svg
+>
---
+++ src/lib/components/SocialLink.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Component, Snippet } from 'svelte';
+	import Link from './Link.svelte';
+
+	let {
+		href,
+		icon: Icon,
+		children
+	}: { href: string; icon: Component; children: Snippet } = $props();
+</script>
+
+<Link {href} class="group">
+	<span class="sr-only">{@render children?.()}</span>
+	<Icon
+		class="h-5 w-5 fill-slate-700 transition group-hover:fill-slate-900 dark:group-hover:fill-slate-500"
+	/>
+</Link>

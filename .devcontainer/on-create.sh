c7e9dd50e52debbd8a0088a31c1662909e1dcf0f
Matthew Gibbons <mhgibbons@me.com>
2025-09-17 05:43:49 +0000

    Add devcontainer configuration and update scripts for environment setup

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export HOME="/home/vscode"
+export ENV="$HOME/.bashrc"
+export SHELL="$(which bash)" 
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install node
+
+wget -qO- https://get.pnpm.io/install.sh | bash -
+
+export PNPM_HOME="/home/vscode/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+pnpm add --global --allow-build=esbuild --allow-build=nx --allow-build=sharp --allow-build=workerd nx@latest wrangler@latest
